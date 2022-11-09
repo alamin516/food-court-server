954a1ffc6f0f1fed1cd19dfd263adfde5619cc09
Md Alamin <alaminduc516@gmail.com>
2022-11-10 03:12:11 +0600

    POST service on mongodb

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { config } = require('dotenv');
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 // middle waers
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ts14m7y.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const foodCollections = client.db('foodCourt').collection('services')
+
+        
+
+        app.post('/service', async (req, res)=>{
+            const service = req.body;
+            const result = await foodCollections.insertOne(service);
+            res.send(result)
+            console.log(result);
+        })
+           
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
+
+app.get('/', (req, res) => {
     res.send("Food COURT SERVER IS RUNNING")
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Food court is running on ${port}`);
 })
